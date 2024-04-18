2fc3434c8a9f9cd79845b2193ae7617d1abe4f88
aryaayy <aryayyy9604@gmail.com>
2024-04-18 19:28:53 +0700

    added welcome, signup, login page and image assets. modified pubspec and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sehatyuk/ProfilePage.dart';
+import 'package:sehatyuk/welcome.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,9 +17,23 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const ProfilePage(),
+      home: const WelcomePage(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFF4A707A),
+          secondary: Color(0xFFC2C8C5),
+          tertiary: Color(0xFF94B0B7),
+          onPrimary: Color(0xFF37363B)
+        ),
+        fontFamily: 'Poppins'
+      ),
     );
   }
+}
+
+mixin AppMixin{
+  FontWeight bold = FontWeight.w700;
+  FontWeight semi = FontWeight.w600;
+  double sideMargin = 20;
 }
