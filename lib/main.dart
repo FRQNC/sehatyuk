866c10b75b0987a4695a6f7bbeaaa2881a2cdde5
FRQNC <alhaqqi123@gmail.com>
2024-04-21 00:01:22 +0700

    resolved conflict

--- lib/main.dart
+++ lib/main.dart
@@ -97,69 +97,6 @@ class BulletList extends StatelessWidget {
           );
         }).toList(),
       ),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en', ''),
-        Locale('id', 'ID'), // arabic, no country code
-      ],
-    );
-  }
-}
-
-class BulletList extends StatelessWidget {
-  final List<String> strings;
-  final double fontSize;
-  final FontWeight fontWeight;
-  final Color color;
-  final double letterSpacing;
-
-  BulletList(this.strings, this.fontSize, this.fontWeight, this.color, this.letterSpacing);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.centerLeft,
-      // padding: EdgeInsets.fromLTRB(0, 15, 16, 0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: strings.map((str) {
-          return Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '\u2022',
-                style: TextStyle(
-                  fontSize: this.fontSize,
-                  fontWeight: this.fontWeight,
-                  color: this.color,
-                ),
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              Expanded(
-                child: Container(
-                  child: Text(
-                    str,
-                    textAlign: TextAlign.left,
-                    softWrap: true,
-                    style: TextStyle(
-                      fontSize: this.fontSize,
-                      fontWeight: this.fontWeight,
-                      color: this.color,
-                      letterSpacing: this.letterSpacing,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          );
-        }).toList(),
-      ),
     );
   }
 }
