6e4c739a18d6b0180474ca4c3d33ea75bac9b6dd
unknown <shshumay@gmail.com>
2024-05-13 22:46:03 +0700

    add Obat model and provider

---
+++ lib/models/obat.dart
@@ -0,0 +1,38 @@
+class Obat{
+  int idObat;
+  String namaObat;
+  String deskripsiObat;
+  String komposisiObat;
+  String dosisObat;
+  String peringatanObat;
+  String efekSampingObat;
+  String fotoObat;
+  int idJenisObat;
+
+  // Constructor
+  Obat({
+    required this.idObat,
+    required this.namaObat,
+    required this.deskripsiObat,
+    required this.komposisiObat,
+    required this.dosisObat,
+    required this.peringatanObat,
+    required this.efekSampingObat,
+    required this.fotoObat,
+    required this.idJenisObat,
+  });
+
+   factory Obat.fromJson(Map<String, dynamic> json) {
+    return Obat(
+      idObat: json['id_obat'],
+      namaObat: json['nama_obat'],
+      deskripsiObat: json['deskripsi_obat'],
+      komposisiObat: json['komposisi_obat'],
+      dosisObat: json['dosis_obat'],
+      peringatanObat: json['peringatan_obat'],
+      efekSampingObat: json['efek_samping_obat'],
+      fotoObat: json['foto_obat'],
+      idJenisObat: json['id_jenis_obat'],
+    );
+   }
+}
