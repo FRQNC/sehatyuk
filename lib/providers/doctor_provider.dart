7f3b59ed06e703acd61b9ec502da7b4795c2ae1f
unknown <shshumay@gmail.com>
2024-05-13 20:59:42 +0700

    add Doctor model and provider

--- lib/providers/doctor_provider.dart
+++ lib/providers/doctor_provider.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:sehatyuk/models/doctor.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class DoctorProvider extends ChangeNotifier{
-  
+class DoctorProvider extends ChangeNotifier {
+  List<Doctor> _doctors = [];
+
+  List<Doctor> get doctors => _doctors;
+
+  set doctors(List<Doctor> value) {
+    _doctors = value;
+    notifyListeners();
+  }
+
+  Future<void> fetchData(String token) async {
+    try {
+      final url = Uri.parse(''); // url read doctornya
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = json.decode(response.body);
+        _doctors = responseData.map((data) => Doctor.fromJson(data)).toList();
+        notifyListeners(); // Memberi tahu pendengar tentang perubahan pada data
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (error) {
+      print('Error: $error');
+      notifyListeners(); // Memberi tahu pendengar bahwa terjadi kesalahan
+      // Handle error sesuai dengan kebutuhan aplikasi Anda
+    }
+  }
 }
