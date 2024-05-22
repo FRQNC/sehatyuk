7e4237fe38f78da1461ebc4c81221d40f33828c5
FRQNC <alhaqqi123@gmail.com>
2024-05-22 22:36:28 +0700

    changed endpoint.dart to support both web and android

--- lib/providers/endpoint.dart
+++ lib/providers/endpoint.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+
 class Endpoint{
-  static String url = "http://10.0.2.2:8000/";
+  static String host = kIsWeb ? "127.0.0.1" : "10.0.2.2";
+  static String url = "http://$host:8000/";
 }
