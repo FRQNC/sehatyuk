a41a57f412b7cc0055307a85d1f98051351d0eee
FRQNC <alhaqqi123@gmail.com>
2024-06-12 09:57:04 +0700

    connected to hosted webservice

--- lib/providers/endpoint.dart
+++ lib/providers/endpoint.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 
 class Endpoint {
-  static String host = kIsWeb ? "127.0.0.1" : "10.0.2.2";
-  static String url = "http://$host:8000/";
+  static String url = "https://sehatyuk-webservice.onrender.com/";
 }
